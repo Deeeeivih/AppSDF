ed61e19e606f92800a7ea450856cc66e9459fb04
Deeeeivih <59234495+Deeeeivih@users.noreply.github.com>
2020-10-07 21:24:31 -0300

    Modulo login

--- app/src/main/java/com/example/appsdf/MainActivity.java
+++ app/src/main/java/com/example/appsdf/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.appsdf;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText txtuser, txtpass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtuser = findViewById(R.id.txtuser);
+        txtpass = findViewById(R.id.txtpass);
+    }
+
+    public void iniciar(View view) {
+        if (txtuser.getText().toString().isEmpty() || txtpass.getText().toString().isEmpty()) {
+            Toast.makeText(this, "Complete Campos!", Toast.LENGTH_LONG).show();
+
+        } else {
+            Intent i = new Intent(this, PrincipalActivity.class);
+            startActivity(i);
+            String user = txtuser.getText().toString();
+            Toast.makeText(this, "Hello " + user, Toast.LENGTH_SHORT).show();
+            txtuser.setText("");
+            txtpass.setText("");
+        }
     }
 }
