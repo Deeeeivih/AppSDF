cd8a2c937aee10c296677f6787176757b24425de
Deeeeivih <59234495+Deeeeivih@users.noreply.github.com>
2020-10-08 23:48:17 -0300

    Modifique JavaClass News

--- app/src/main/java/com/example/appsdf/model/News.java
+++ app/src/main/java/com/example/appsdf/model/News.java
@@ -2,4 +2,15 @@ package com.example.appsdf.model;
 
 public class News {
 
+    public String name;
+    public String author;
+    public String title;
+    public String urlToImage;
+
+    public News(String name, String author, String title, String urlToImage) {
+        this.name = name;
+        this.author = author;
+        this.title = title;
+        this.urlToImage = urlToImage;
+    }
 }
